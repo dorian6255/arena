a4589a73946a4d2dd1e148b51017c493fc186d5a
Sira <aymeric.brunin59@gmail.com>
2024-03-29 17:29:13 +0100

    fix chara builder + start round system

--- cmd/arena/arena.go
+++ cmd/arena/arena.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"log"
+	"os"
 )
 
 type player struct {
@@ -29,31 +30,55 @@ const MAX_ALLO_STATS = 27 //Based on baldur's gate 3 character creator
 
 func main() {
 	fmt.Println("Welcome to the Arena")
-	player, err := getPlayerStats()
+	player := getPlayerStats()
+	if err != nil {
+		log.Fatal(err)
+	}
+	roundNumber := 1
+	for {
+		if ok := fightRound(player, roundNumber); !ok {
+			break
+		}
+		roundNumber++
+	}
+
 }
 
-func getPlayerStats() (player, error) {
+func getPlayerStats() player {
 	baseCharacter := newPlayer()
 	availablePoints := MAX_ALLO_STATS
 	fmt.Println("Your character starts with 8 points in every of those stastistics :")
 	fmt.Println("STRENGTH DEXTERITY CONSTITUTION INTELLIGENCE WISDOM AND CHARISMA")
 	fmt.Println("You will now be asked to allocate %s points to your statistics", availablePoints)
 	fmt.Println("Your base statistics can't go beyond 15 ")
-	for i := 0; i < reflect.TypeOf(&baseCharacter).NumField(); i++ {
-		field := reflect.TypeOf(&baseCharacter).Field(i)
-		printInputRequest(field.Name, availablePoints)
-	}
-	return baseCharacter, nil
+	input := printInputRequest("strength", &availablePoints)
+	baseCharacter.STR += input
+	input = printInputRequest("dexterity", &availablePoints)
+	baseCharacter.DEX += input
+	input = printInputRequest("constitution", &availablePoints)
+	baseCharacter.CON += input
+	input = printInputRequest("intelligence", &availablePoints)
+	baseCharacter.INTE += input
+	input = printInputRequest("wisdom", &availablePoints)
+	baseCharacter.WIS += input
+	input = printInputRequest("charisma", &availablePoints)
+	baseCharacter.CHA += input
+	return baseCharacter
 }
 
-func printInputRequest(statName string, availablePoints int) (int, int, error) {
+func printInputRequest(statName string, availablePoints *int) int {
 	fmt.Println("Please enter the number of points your want to add to your %s", statName)
 	fmt.Println("Remaining points : %s", availablePoints)
 	var input int
 	_, err := fmt.Scan(&input)
-	if err != nil || input > 7 || input > availablePoints {
-		return 0, 0, err
+	if err != nil || input > 7 || input > *availablePoints {
+		log.Fatal(err)
+		os.Exit(2)
 	}
-	availablePoints -= 7
-	return input, availablePoints, nil
+	*availablePoints -= input
+	return input
+}
+
+func fightRound(player player, roundNumber int) bool {
+
 }
