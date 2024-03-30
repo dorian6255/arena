a96ad184ec7787d7f897eab3d4d74451ce6c4f2d
Dorian <dorian.6255@hotmail.fr>
2024-03-30 18:37:43 +0100

    push modifs on handlers

---
+++ internal/handlers/baseHandler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/dorian6255/arena/internal/gameLoop"
+	"log/slog"
+)
+
+type Handler interface {
+	Start()
+	Stop()
+	Init()
+}
+
+type BaseHandler struct {
+	game    *gameLoop.GameLoop
+	outputs []*gameLoop.OuputAdapter
+}
+
+// init the handler by allowing adapter & gameLoop
+func (b *BaseHandler) InitGameLoop() {
+	b.game = &gameLoop.GameLoop{}
+	b.game.Init()
+	//TODO
+
+}
+
+func (b *BaseHandler) InitOutputAdapter(adapters ...gameLoop.OuputAdapter) {
+
+	for _, adapter := range adapters {
+		err := adapter.Init()
+		fmt.Printf("TODO, DO SMTH WITH ERROR %v ", err)
+		b.outputs = append(b.outputs, &adapter)
+	}
+	slog.Info("OutputAdapter Initated")
+}
